92b0a39229956fed1240bfc7f1d5560a33c46b77
Javier Gutierrez <javier.guse@icloud.com>
2020-03-14 16:58:02 -0300

    create program to find even or odd numbers

---
+++ even_or_odd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	min := 0
+	max := 11
+	var numbers []int
+
+	for a := min; a < max; a++ {
+		numbers = append(numbers, a)
+	}
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Printf("%v is even", number)
+		} else {
+			fmt.Printf("%v is odd", number)
+		}
+	}
+
+	// fmt.Println(numbers)
+}
